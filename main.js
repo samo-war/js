27958112e6be4debeeff3dc7eb11bfaf2467f203
samo-war <samo_war@mail.ru>
2019-04-23 10:22:46 +0300

    feat: add if

--- main.js
+++ main.js
@@ -1,24 +1,83 @@
-const money = prompt('Ваш бюджет на месяц?');
-const time = prompt('Введите дату в формате YYYY-MM-DD');
-const exp1 = prompt('Введите обязательную статью расходов в этом месяце');
-const exp2 = prompt('Во сколько обойдется?');
-const exp3 = prompt('Введите обязательную статью расходов в этом месяце');
-const exp4 = prompt('Во сколько обойдется?');
+const money = +prompt('Ваш бюджет на месяц?', '');
+const time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 const appData = {
-  moneyData: money,
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
   timeData: time,
-  expenses: {
-    exp1: exp2,
-    exp3: exp4
-  },
-  optionalExpenses: {
-
-  },
-  income: [
-
-  ],
   savings: false
 };
-alert(appData.moneyData / 30);
-console.log(appData.expenses.exp1);
+
+// let i = 0;
+// while (i < 2) {
+//   const a = prompt('Введите обязательную статью расходов в этом месяце', '');
+//   const b = prompt('Во сколько обойдется?', '');
+
+//   if (typeof(a) === 'string'
+//       && a !== null
+//       && a !== ''
+//       && parseInt(b) !== NaN
+//       && b !== null
+//       && b !== '') {
+//     appData.expenses[a] = parseInt(b);
+//     console.log('done');
+//   } else {
+//     --i;
+//   }
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   const a = prompt('Введите обязательную статью расходов в этом месяце', '');
+//   const b = prompt('Во сколько обойдется?', '');
+
+//   if (typeof(a) === 'string'
+//       && a !== null
+//       && a !== ''
+//       && parseInt(b) !== NaN
+//       && b !== null
+//       && b !== '') {
+//     appData.expenses[a] = parseInt(b);
+//     console.log('done');
+//   } else {
+//     --i;
+//   }
+//   i++;
+// } while (i < 2);
+
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Введите обязательную статью расходов в этом месяце', '');
+  const b = prompt('Во сколько обойдется?', '');
+
+  if (typeof(a) === 'string'
+      && a !== null
+      && a !== ''
+      && parseInt(b) !== NaN
+      && b !== null
+      && b !== '') {
+    appData.expenses[a] = parseInt(b);
+    console.log('done');
+  } else {
+    --i;
+  }
+}
+
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert(`Ежедневный бюджет: ${appData.moneyPerDay}`);
+
+if (appData.moneyPerDay < 100) {
+  console.log('У Вас минимальный прожиточный бюджет');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('У Вас средний бюджет');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('У Вас высокий бюджет');
+} else {
+  console.log('Произошла ошибка');
+} 
